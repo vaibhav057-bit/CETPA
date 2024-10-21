f42ff6a0ca9e993f07b00a526fbcc498ec41b18e
Vaibhav Vashisth <vaibhavvashisth057@gmail.com>
2024-10-21 10:54:05 +0530

    Add files via upload

---
+++ Javascript/Assignment 2/index.js
@@ -0,0 +1,122 @@
+//  No. 1  ##################################################### Finding grades
+
+// function Find_grades(){
+//     var M = 8;
+//     switch(true){
+//         case(41<= M && M<= 50):
+//                 console.log("A");
+//                 break;
+//         case(31<= M && M<= 40):
+//                 console.log("B");
+//                 break;
+//         case(21<= M && M<= 30):
+//                 console.log("C");
+//                 break;
+//         case(11<= M && M<= 20):
+//                 console.log("D");
+//                 break;
+//         case(M <= 10):
+//                 console.log("E");
+//                 break;
+//         default:
+//                 console.log("Invalid Marks");
+//                 console.log("Enter the Marks from 0 to 50");
+//                 break;
+//     }
+// }
+// Find_grades();
+
+//  No. 2  #################################################################
+
+// function getValue(){
+
+// let Q = "P";
+
+// if(Q === "P" || Q === "p"){
+//         console.log("PrepBytes");
+// }
+// else if(Q === "Z" || Q === "z"){
+//         console.log("Zenith");
+// }
+// else if(Q === "E" || Q === "e"){
+//         console.log("Expert Coder");
+// }
+// else if(Q === "D" || Q === "d"){
+//         console.log("data Structure");
+// }
+// else{
+//         console.log("Invalid");
+// }
+// }
+// getValue();
+
+// No. 3 ####################################################### finding maximum one
+
+// function Find_max(){
+
+// let e= 2;
+// let f = 5;
+// let g = 4;
+// if(e > f){
+//         if(e > g){
+//                 console.log(e);
+//         }else{
+//                 console.log(g);
+//         }
+// }else{
+//         if(f > g){
+//                 console.log(f);
+//         }else{
+//                 console.log(g);
+//         }
+// }
+// if(e == f && f == g){
+//         console.log("-1");
+// }else{
+//         console.log("unequal");
+// }
+// }
+// Find_max();
+
+//  No. 4 ###################################################second smallest 
+
+// function Sec_Smallest(){
+
+// let x = 5;
+// let y = 11;
+// let z = 23;
+
+// if(x > y){
+//         if(x > z){
+//                 console.log(z);
+//         }else{
+//                 console.log(x);
+//         }
+// }else{
+//         if(y > z){
+//                 console.log(z);
+//         }else{
+//                 console.log(y);
+//         }
+// }
+// }
+// Sec_Smallest();
+
+// No. 5 ##################################################### checking triangle
+
+// function Triangle_Check(){
+
+//         let A = 8;
+//         let B = 10;
+//         let C = 20;
+
+//         let Sum = (A + B + C) ;
+//         if(Sum < 90){
+//                 console.log("Acute");
+//         }else if(Sum = 90){
+//                 console.log("Right Angle Triangle");
+//         }else{
+//                 console.log("Obtuse");
+//         }
+// }
+// Triangle_Check();
